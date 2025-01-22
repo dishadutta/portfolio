034aaad8a089dad38f94dcb6fb4c87c00d72c4bc
dishadutta <duttadisha12345@gmail.com>
2025-01-22 20:12:01 +0530

    feat: fixed header at the top of the page

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,10 +18,14 @@ export default function RootLayout({
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang='en'>
-      <body className='font-serif flex flex-col xl:px-96 md:px-48 sm:px-32 px-3 lg:px-50'>
+      <body className='font-serif flex flex-col'>
         <ThemeProvider>
-          <Navbar />
-          {children}
+          <div className='fixed top-0 left-0 w-full z-50 bg-inherit xl:px-96 md:px-40 sm:px-28 px-3 lg:px-50'>
+            <Navbar />
+          </div>
+          <div className='pt-28 xl:px-96 md:px-40 sm:px-28 px-3 lg:px-50 z-0'>
+            {children}
+          </div>
           <Footer />
         </ThemeProvider>
       </body>
